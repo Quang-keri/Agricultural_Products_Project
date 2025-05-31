c129db3ff5521852603cf62f02728ea56aa86dc3
Quang-keri <phamquang040102@gmail.com>
2025-05-31 15:46:13 +0700

    set up project

---
+++ src/main/java/hsf302/agricultural_products_project/AgriculturalProductsProjectApplication.java
@@ -0,0 +1,13 @@
+package hsf302.agricultural_products_project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AgriculturalProductsProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AgriculturalProductsProjectApplication.class, args);
+    }
+
+}
---
+++ src/test/java/hsf302/agricultural_products_project/AgriculturalProductsProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package hsf302.agricultural_products_project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AgriculturalProductsProjectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

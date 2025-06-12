6d69f4772e6a10bfeb3931cdd98c200eb9425c2d
sondx24 <sondxse181792@fpt.edu.vn>
2025-06-12 14:31:01 +0700

    memberlist

--- src/main/java/hsf302/agricultural_products_project/controller/AdminController.java
+++ src/main/java/hsf302/agricultural_products_project/controller/AdminController.java
@@ -1,8 +1,11 @@
 package hsf302.agricultural_products_project.controller;
 
 import hsf302.agricultural_products_project.model.User;
+import hsf302.agricultural_products_project.service.CustomUserDetails;
 import hsf302.agricultural_products_project.service.UserServiceImpl;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -20,6 +23,17 @@ public class AdminController {
 
     @GetMapping("/")
     public String adminDashboard(Model model) {
+
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        if (authentication != null && authentication.isAuthenticated()
+                && authentication.getPrincipal() instanceof CustomUserDetails) {
+
+            CustomUserDetails userDetails = (CustomUserDetails) authentication.getPrincipal();
+            User user = userDetails.getUser();
+            System.out.println("User: " + user.getUserName() + ", Role: " + user.getRole());
+            model.addAttribute("user", user);
+        }
+
         List<User> members = userService.findAll();
         model.addAttribute("members", members);
         return "admin/admin";

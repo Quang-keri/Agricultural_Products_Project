57afd820dd3c1513a578eb1b33d18ec551be64e1
sondx24 <sondxse181792@fpt.edu.vn>
2025-06-12 14:27:35 +0700

    memberlist

--- src/main/java/hsf302/agricultural_products_project/controller/AdminController.java
+++ src/main/java/hsf302/agricultural_products_project/controller/AdminController.java
@@ -1,30 +1,42 @@
 package hsf302.agricultural_products_project.controller;
 
 import hsf302.agricultural_products_project.model.User;
-import hsf302.agricultural_products_project.service.CustomUserDetails;
-import org.springframework.security.core.Authentication;
-import org.springframework.security.core.context.SecurityContextHolder;
+import hsf302.agricultural_products_project.service.UserServiceImpl;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.List;
 
 @Controller
 @RequestMapping("/admin")
 public class AdminController {
 
-     @GetMapping("/")
+    @Autowired
+    private UserServiceImpl userService;
+
+    @GetMapping("/")
     public String adminDashboard(Model model) {
-         Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
-         if (authentication != null && authentication.isAuthenticated()
-                 && authentication.getPrincipal() instanceof CustomUserDetails) {
+        List<User> members = userService.findAll();
+        model.addAttribute("members", members);
+        return "admin/admin";
+    }
 
-             CustomUserDetails userDetails = (CustomUserDetails) authentication.getPrincipal();
-             User user = userDetails.getUser();
-             System.out.println("User: " + user.getUserName() + ", Role: " + user.getRole());
-             model.addAttribute("user", user);
-         }
-         return "admin/admin";
-     }
-
+    @GetMapping("/update-status/{id}")
+    public String updateStatus(@PathVariable("id") Long id) {
+        User user = userService.findById(id);
+        if (user != null) {
+            String currentStatus = user.getStatus();
+            if ("active".equalsIgnoreCase(currentStatus)) {
+                user.setStatus("deactive");
+            } else {
+                user.setStatus("active");
+            }
+            userService.save(user);
+        }
+        return "redirect:/admin/";
+    }
 }

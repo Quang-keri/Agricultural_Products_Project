3e3685bc3ba9ce28a0fad318dd95403bc7ad6cff
Thai <lqthai17092004@gmail.com>
2025-06-18 14:22:56 +0700

    update

--- src/main/java/hsf302/agricultural_products_project/service/UserService.java
+++ src/main/java/hsf302/agricultural_products_project/service/UserService.java
@@ -3,7 +3,13 @@ package hsf302.agricultural_products_project.service;
 import hsf302.agricultural_products_project.dto.UserDTO;
 import hsf302.agricultural_products_project.model.User;
 
+import java.util.List;
+
 public interface UserService {
     User save(UserDTO user);
     User findByUserName(String userName);
+    List<User> findAll();
+    User findById(Long id);
+    User save(User user);
+    void updateStatus(Long id);
 }

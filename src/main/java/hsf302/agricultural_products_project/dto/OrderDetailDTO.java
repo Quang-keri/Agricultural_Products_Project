b9ef2dda4025f2dcb56e7211bf46702df3bc2dda
lhducc <pduc2200@gmail.com>
2025-07-18 04:21:09 +0700

    add view detail for oderManagement and history

--- src/main/java/hsf302/agricultural_products_project/dto/OrderDetailDTO.java
+++ src/main/java/hsf302/agricultural_products_project/dto/OrderDetailDTO.java
@@ -6,4 +6,34 @@ public class OrderDetailDTO {
     private String productName;
     private int quantity;
     private BigDecimal totalPrice;
+
+    public OrderDetailDTO(String productName, BigDecimal totalPrice, int quantity) {
+        this.productName = productName;
+        this.totalPrice = totalPrice;
+        this.quantity = quantity;
+    }
+
+    public String getProductName() {
+        return productName;
+    }
+
+    public void setProductName(String productName) {
+        this.productName = productName;
+    }
+
+    public int getQuantity() {
+        return quantity;
+    }
+
+    public void setQuantity(int quantity) {
+        this.quantity = quantity;
+    }
+
+    public BigDecimal getTotalPrice() {
+        return totalPrice;
+    }
+
+    public void setTotalPrice(BigDecimal totalPrice) {
+        this.totalPrice = totalPrice;
+    }
 }

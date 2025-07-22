4f3d5f4bf48e9aa4503200ac1fc8e82fe569dd90
Tri <quachtri865@gmail.com>
2025-07-22 14:32:20 +0700

    Refactor product addition: update add-to-cart functionality with form submission and change product link in cart

--- src/main/java/hsf302/agricultural_products_project/model/User.java
+++ src/main/java/hsf302/agricultural_products_project/model/User.java
@@ -20,7 +20,7 @@ public class User {
     @Column(name = "user_id")
     private Long userId;
 
-    @Column(name = "user_name", columnDefinition = "NVARCHAR(50)",unique = true)
+    @Column(name = "user_name", columnDefinition = "NVARCHAR(50)")
     private String userName;
 
     @Column(name = "full_name", columnDefinition = "NVARCHAR(100)")
